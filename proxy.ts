c88b95543abf99e0af78d723726089129302836b
Elias <github@northernsi.de>
2023-02-18 18:38:05 +0100

    💽 Load from JSON & Log

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,48 @@
+import {Database} from "bun:sqlite";
+import process from "node:process";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const hosts = require("./hosts.json"),
+    db = Database.open("lyra.sqlite", {
+        readonly: false,
+        create: true
+    });
+
+db.run(`CREATE TABLE IF NOT EXISTS requests ("origin" json NOT NULL, "target" json NOT NULL);`);
+
 Bun.listen({
     hostname: "localhost",
-    port: 8080,
+    port: parseInt(process.env.LYRA_PORT),
     socket: {
-        data(socket, data) {
-            Bun.connect({
-                hostname: "localhost",
-                port: 1234,
-                socket: {
-                    open(_socket) {
-                        console.log(`[TEST] ${data.toString()}`)
-                        _socket.write(data.toString());
-                    },
-                    data(_socket, message) {
-                        console.log(`[INCOMING] > ${message.toString()}`);
-                        socket.write(message.toString());
-                    }
-                }
-            });
+        data(client, data) {
+            const host = hosts.filter(host => host.origin.host == client.listener.hostname && host.origin.port == client.listener.port)[0]
+            if (host) {
+                Bun.connect({
+                    hostname: host.target.host,
+                    port: host.target.port,
+                    socket: {
+                        open(server) {
+                            console.log(`[CLIENT -> SERVER] > ${data.toString()}`);
+                            server.write(data.toString());
+                        },
+                        data(_socket, message) {
+                            const origin = JSON.stringify({
+                                host: client.listener.hostname,
+                                port: client.listener.port
+                            }), target = JSON.stringify({
+                                host: host.target.host,
+                                port: host.target.port
+                            });
 
-            console.log(`[INCOMING] > ${data.toString()}`);
+                            db.run(`INSERT INTO requests VALUES ($origin, $target)`, {$origin: origin, $target: target});
+                            console.log(`[SERVER -> CLIENT] > ${message.toString()}`);
+                            client.write(message.toString());
+                        }
+                    }
+                });
+            }
         }
     }
 });
--- server.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    port: 1234,
-    fetch(request) {
-        console.log(request)
-        return new Response("Welcome to Bun!");
-    },
-};
